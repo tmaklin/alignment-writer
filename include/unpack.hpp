6daf9757a78e0e0b6db0fd4b04e28f447a55b974
Tommi Mäklin <tommi@maklin.fi>
2023-01-02 15:22:06 +0200

    Declare the second DeserializeBuffer.

--- include/unpack.hpp
+++ include/unpack.hpp
@@ -51,6 +51,7 @@ void UnpackPlain(std::istream *in, std::ostream *out);
 
 // Deserialize one section of data written with BufferedPack
 void DeserializeBuffer(std::istream *in, bm::bvector<> *out);
+void DeserializeBuffer(const size_t buffer_size, std::istream *in, bm::bvector<> *out);
 
 // Function for reading the header line of the alignment file
 void ReadHeader(std::istream *in, size_t *n_reads, size_t *n_refs);
